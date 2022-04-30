82c89cc026e0dbba62a9c467e325b572903c43ee
Kelvin Garcia <kelvinrgarcia98@gmail.com>
2022-04-30 20:30:14 -0300

    project setup

---
+++ src/main/java/com/kelvin/casepix/CasePixApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.casepix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasePixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasePixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelvin/casepix/CasePixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.casepix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasePixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
